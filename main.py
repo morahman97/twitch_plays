65c7942d24581f9d9a15ea32ff90965339ee5e7f
Mo Rahman <morahman97@gmail.com>
2021-07-16 22:00:35 -0700

    increased block duration

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def press(key):
     delay = 0.2
     # If input command is to move, set a higher duration for movement
     if key == 'forward' or key == 'left' or key == 'back' or key == 'right' or key == 'l1':
-        delay = 0.5
+        delay = 0.75
     if key in dodgeSet:
         dir = key[5:] # extract dodge direction
         dodge(dir)
